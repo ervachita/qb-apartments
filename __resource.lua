13c26fdf4cedff3b77c112d9f82d7b0c1c4d4092
Ervachita <73184212+ervachita@users.noreply.github.com>
2020-10-20 21:22:05 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,17 @@
+-- Converted by xaneh#6591
+-- İf you want more qb scripts =  https://discord.gg/wBNwPCv
+
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+description 'Qbus:Apartments'
+
+server_scripts {
+	"server/main.lua",
+	"config.lua",
+}
+
+client_scripts {
+	"client/main.lua",
+	"client/gui.lua",
+	"config.lua",
+}
